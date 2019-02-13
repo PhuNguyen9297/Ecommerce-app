e4d8ca0fc0bc1404a6c8888708d995b51389d06c
PhuNguyen9297 <PhuNguyen9297@gmail.com>
2019-02-13 11:25:15 +0700

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Switch,Route} from 'react-router-dom';
+import ProductList from "./Components/ProductList";
+import Detail from "./Components/Detail";
+import Navbar from "./Components/Navbar";
+import Cart from "./Components/Cart";
+import Default from "./Components/Default";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProductList}></Route>
+          <Route path="/detail" component={Detail} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
       </div>
     );
   }
