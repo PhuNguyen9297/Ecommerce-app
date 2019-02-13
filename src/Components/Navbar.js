c6c5bd1e2c2a828e93724e6f2ee76bf3e4e610b4
PhuNguyen9297 <PhuNguyen9297@gmail.com>
2019-02-13 11:30:33 +0700

    navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,8 +3,12 @@ import React, { Component } from 'react'
 export default class Navbar extends Component {
   render() {
     return (
-      <div>
-        Navbar
+      <div className="container">
+        <div className="row">
+          <div className="col-1">icon</div>
+          <div className="col-2">Product</div>
+          <div className="col-2">My Cart</div>
+        </div>
       </div>
     )
   }
